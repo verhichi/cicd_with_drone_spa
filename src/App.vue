d4c7a1b9d62214ff4b14c978002a02983a6cf05a
Daichi Nishida <verhichi@hotmail.com>
2019-12-22 13:38:29 +0900

    edit App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="WOOOOOOOOOOOOOOOOOOs"/>
+    <HelloWorld msg="JASDLK>JASDKJASL+DKAJ+LK"/>
   </div>
 </template>
 
